fda5395f625b6a5814eaefb863f3e8cd658a01c4
SungYoon97 <f4boa@naver.com>
2019-09-04 17:44:18 +0900

    2019.09.04. 수

---
+++ src/food2/FoodVO.java
@@ -0,0 +1,53 @@
+package food2;
+
+public class FoodVO {
+	private String name;
+	private String menu;
+	private String price;
+	private String loc;
+	private String star;
+	private String tel;
+	private String time;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public String getStar() {
+		return star;
+	}
+	public void setStar(String star) {
+		this.star = star;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
